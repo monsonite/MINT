ef636675b7537c529489a7b10d0838fbc637f13c
John Hardy <jh@lagado.com>
2021-12-01 11:59:17 +1100

    macros

--- MINT-macros.asm
+++ MINT-macros.asm
@@ -1,21 +1,22 @@
 empty_:
         .cstr ";"
 
-; escape_:
-;         ; .cstr ";"
-;         .cstr "13\\E70(` `)13\\E`> `0\\$!;"
-
 backsp_:
         .cstr "\\$@0=0=(1\\$\\-8\\E` `8\\E);"
 
-toggleBase_:
-        .cstr "\\b@0=\\b!;"
-
-printStack_:
-        .cstr "`=> `\\P\\N\\N`> `;"        
+edit_:
+        .cstr "`?`\\K\\N`> `\\^A-\\Z;"
 
 list_:
         .cstr "\\N26(\\i@\\Z\\$@0>(\\N))\\N`> `;"
 
-edit_:
-        .cstr "`?`\\K\\N`> `\\^A-\\Z;"
+printStack_:
+        .cstr "`=> `\\P\\N\\N`> `;"        
+
+toggleBase_:
+        .cstr "\\B@0=\\B!;"
+
+; escape_:
+;         ; .cstr ";"
+;         .cstr "13\\E70(` `)13\\E`> `0\\$!;"
+
