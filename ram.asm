c4b4462cdedfd20c7ae4f4485b13294af4bbb7f5
John Hardy <jh@lagado.com>
2021-12-10 23:42:24 +1100

    ram

--- ram.asm
+++ ram.asm
@@ -7,8 +7,21 @@ dStack:
             DS RSIZE
 rStack:        
 
-TIB:        
-            DS TIBSIZE
+TIB:        DS TIBSIZE
+BUF:        DS $80
+
+RST08:      DW 0                ; 
+RST10:      DW 0                ; 
+RST18:      DW 0                ; 
+RST20:      DW 0                ; 
+RST28:      DW 0                ; 
+RST30:      DW 0                ; 
+BAUD        DW 0                ; 
+INTVEC:     DW 0                ; 
+NMIVEC:     DW 0                ; 
+GETCVEC:    DW 0                ;   
+PUTCVEC:    DW 0                ;   
+tbPtr:      DW 0                ; reserved for tests
 
             .align $100
 mintVars:
@@ -58,6 +71,9 @@ vHeapPtr:   DW 0                ; h
             DW 0                ; y
             DW 0                ; z
 
+vIFTEMode   DW 0                ; 
+vByteMode:  DW 0                ; 
+
 ; ****************************************************************
 ; VARS Table - holds 26 16-bit user variables
 ; ****************************************************************
@@ -67,24 +83,6 @@ vars:       DS 26 * 2
 ; DEFS Table - holds 26 addresses of user routines
 ; ****************************************************************
 defs:       DS 26 * 2
-
-vIFTEMode   DW 0                ; 
-vByteMode:  DW 0                ; 
-RST08:      DW 0                ; 
-RST10:      DW 0                ; 
-RST18:      DW 0                ; 
-RST20:      DW 0                ; 
-RST28:      DW 0                ; 
-RST30:      DW 0                ; 
-BAUD        DW 0                ; 
-INTVEC:     DW 0                ; 
-NMIVEC:     DW 0                ; 
-GETCVEC:    DW 0                ;   
-PUTCVEC:    DW 0                ;   
-tbPtr:      DW 0                ; reserved for tests
-
-BUF:        DS $80
-
 
             .align $40
 HEAP:         
